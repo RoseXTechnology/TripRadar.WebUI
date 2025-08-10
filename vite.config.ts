ac19542a1ca50310877819e9eaacc40c2d17f235
Oleksandr Hlinka <oleksandr.hlinka@gr8.tech>
2025-08-10 15:58:12 +0300

    add gitHub actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/TripRadar.WebUI/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
