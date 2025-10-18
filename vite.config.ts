12685b7783f2090233d962e4a9542124c1a720da
Oleksandr Hlinka <oleksandr.hlinka@gr8.tech>
2025-10-18 10:18:14 +0300

    fix(auth): fix proxy target for rancher desktop

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5330',
+        target: 'http://127.0.0.1:5330',
         changeOrigin: true,
         secure: false,
       },
